41d9b383e91cec3a9677bd79d2ab7581ae603147
Florencia Perdomo <fperdomoconte@gmail.com>
2024-07-23 18:43:34 -0300

    commit changes

--- js/script.js
+++ js/script.js
@@ -2,13 +2,16 @@
 //Pendiente sanitizacion de datos de entrada
 let menu = prompt("BIENVENIDO! \nDesea calcular el costo de una casa? Ingrese SI / NO")
 
+//Declara y define valor inicial para variable primer dato: metros cuadrados
+let metrosCuadrados = 0
+
+//Declara y define valor inicial para variable segundo dato: tipo de sistema constructivo a utilizar
+let sistemaConstructivo = 0
+
 //Verifica instancia de calculo
 while (menu === "SI") {
-    //Declara y define variable primer dato: metros cuadrados
-    let metrosCuadrados = parseInt(prompt("Ingrese la cantidad de metros cuadrados. \nEjemplo: 100"))
-
-    //Declara y define valor inicial para variable segundo dato: tipo de sistema constructivo a utilizar
-    let sistemaConstructivo = 0
+    //Define valor variable primer dato: metros cuadrados
+    metrosCuadrados = parseInt(prompt("Ingrese la cantidad de metros cuadrados. \nEjemplo: 100"))
 
     //Declara y define variable para sanitizacion de valores de entrada para el segundo dato
     let sistemaValido = false
@@ -19,7 +22,7 @@ while (menu === "SI") {
         //Prompt que solicita al udsuario ingrese el sistema constructivo a utilizar
         sistemaConstructivo = parseInt(prompt("Ingrese el sistema constructivo que usara en su casa: \n1-MAMPOSTERIA TRADICIONAL \n2-PANELES CLT \n3-PANELES SIP \n4-BLOQUES HCCA"))
 
-        //Array que contiene los tipos de sistemas constructivos validos, en un futuro cada sistema puede ser un objeto con informacion del sistema
+        //Array que contiene los tipos de sistemas constructivos validos, en un futuro cada sistema puede ser un objeto con informacion del sistema.
         const sistemasConstructivos = [1, 2, 3, 4]
 
         //Verifica que el valor ingresado por el usuario este incluido en el array que contiene los tipos de sistemas constructivos validos
@@ -34,7 +37,7 @@ while (menu === "SI") {
     //Declara y define valor de base para la variable precioSistema, que cambiara en funcion de la eleccion del usuario
     let precioSistema = 0
 
-    //Define el valor de la variable precioSistema en base al sistema constructivo elegido por el ususuario
+    //Define el valor de la variable precioSistema en base al sistema constructivo elegido por el ususuario. Los precios de los sistemas constructivos podrían en un futuro guardarse en un objeto, simplificando el acceso y reduciendo las condiciones if, else if.
     if (sistemaConstructivo == 1) {
         precioSistema = 950
     } else if (sistemaConstructivo == 2) {
@@ -45,33 +48,27 @@ while (menu === "SI") {
         precioSistema = 1400
     }
 
+    //Declara funcion que calcula el costo de la mano de obra externa + confirm que solicita al usuario confirme si contratara mano de obra externa
+    const calcularManoDeObra = (precioSistema) => precioSistema * 0.30;
+
     //Declara y define valor de base para la variable precioTrabajadores, que cambiara en funcion de la eleccion del usuario
     let precioTrabajadores = 0
 
-    //Confirm que solicita al usuario confirme si contratara mano de obra externa
-    let manodeObra = confirm("Contratara mano de obra externa?")
-    if (manodeObra == true) {
-        /* const calcularMO = (precioSistema) => precioSistema * 0.30 */
-
-        const calcularMO = function () {
-            let porcentajeMO = 0.30
-            precioTrabajadores = precioSistema * porcentajeMO
-        }
-
-        calcularMO ()
+    if (confirm("Contratara mano de obra externa?")) {
+        precioTrabajadores = calcularManoDeObra(precioSistema);
     }
 
     //Define la funcion con el calculo a realizar con los datos ingresados - calcula el costo de la vivienda en base a los m2 y sistema constructivo, le suma un costo fijo de mano de obra (a futuro sera un % de mano de obra contratada vs. autoconstruccion)
-    const calcularCosto = (metrosCuadradosIngresados, precioSistemaElegido, precioManodeObra) => metrosCuadradosIngresados * (precioSistemaElegido + precioManodeObra)
+    const calcularCosto = (metrosCuadrados, precioSistema, precioTrabajadores) => metrosCuadrados * (precioSistema + precioTrabajadores)
 
     //Informa al usuario el costo calculado, concatena texto con variables
     alert("El precio de su casa sera de USD " + calcularCosto(metrosCuadrados, precioSistema, precioTrabajadores))
-    
-    console.log("El precio estimado de su casa fue de USD " + calcularCosto(metrosCuadrados, precioSistema, precioTrabajadores) + ".\nDel costo total, USD " + calcularCosto(metrosCuadrados, precioSistema, 0) + " corresponden a materiales y USD "+ calcularCosto(metrosCuadrados, 0, precioTrabajadores) + " correponden a mano de obra.")    
+
+    console.log("El precio estimado de su casa fue de USD " + calcularCosto(metrosCuadrados, precioSistema, precioTrabajadores) + ".\nDel costo total, USD " + calcularCosto(metrosCuadrados, precioSistema, 0) + " corresponden a materiales y USD " + calcularCosto(metrosCuadrados, 0, precioTrabajadores) + " correponden a mano de obra.")
 
     //Consulta al usuario si desea realizar otro calculo
     //Pendiente sanitizacion de datos de entrada
     menu = prompt("Desea hacer otra estimacion? Ingrese SI / NO")
 
 } //Informa que se esta cerando el simulador 
-alert("Cerrando calculadora!")
+alert("Cerrando simulador!")
